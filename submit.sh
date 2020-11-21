37799ad87156b13221f059047ecb88ec388b1e53
Joan Rosell <joan.rosellr@gmail.com>
2020-11-21 23:42:18 +0100

    update submission script to use positional parameters

--- submit.sh
+++ submit.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -l
 #SBATCH --exclusive
 #SBATCH -p aolin.q
+# Parameters:
+#   $1 Epochs
+#   $2 seed
 
 hostname
 echo
@@ -9,5 +12,5 @@ echo
 module add gcc/9.2.0
 gcc -Ofast TSP.c -o TSP
 
-perf stat -d TSP 2>&1
-perf record TSP 2>&1
+perf stat -d TSP $1 $2 2>&1
+perf record TSP $1 $2 2>&1
